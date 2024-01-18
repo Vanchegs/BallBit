cb1c006a156d484512ebfc03df0cbc209d118f4d
Vanchegs <manshin9300@gmail.com>
2024-01-18 09:44:57 +0300

    fix Randomization Start Position

--- Assets/Internal/Codebase/BalloonLogic/Balloon.cs
+++ Assets/Internal/Codebase/BalloonLogic/Balloon.cs
@@ -7,16 +7,34 @@ namespace Internal.Codebase.BalloonLogic
     {
         private const float ConstantSpeed = 0.05f;
 
+        private BalloonFactory balloonFactory;
+
         [SerializeField] private Transform balloonTransform;
 
-        private void Start() => RandomizationStartPosition();
+        private void Start()
+        {
+            balloonFactory = FindObjectOfType<BalloonFactory>();
+            
+            RandomizationStartPosition();
+        }
 
-        private void FixedUpdate() => ConstantMoveUp();
+        private void FixedUpdate()
+        {
+            ConstantMoveUp();
+            CheckDeleteHeight();
+        }
 
         private void ConstantMoveUp()
         {
             var input = new Vector3(0, ConstantSpeed, 0);
             balloonTransform.Translate(input);
+        }
+
+        private void CheckDeleteHeight()
+        {
+            if (!(balloonTransform.position.y > 10)) return;
+            balloonFactory.BalloonPool.ReturnToPool(this);
+            RandomizationStartPosition();
         }
 
         private void RandomizationStartPosition()
--- Assets/Internal/Codebase/BalloonLogic/BalloonFactory.cs
+++ Assets/Internal/Codebase/BalloonLogic/BalloonFactory.cs
@@ -8,14 +8,14 @@ namespace Internal.Codebase.BalloonLogic
         private const byte PoolSize = 50;
         private const int SpawnRate = 1;
 
-        private BalloonPool<Balloon> balloonPool;
+        public BalloonPool<Balloon> BalloonPool;
 
         [SerializeField] private Balloon balloon;
         [SerializeField] private Transform balloonContainer;
 
         private void Start()
         {
-            balloonPool = new BalloonPool<Balloon>(balloon, balloonContainer, PoolSize, true);
+            BalloonPool = new BalloonPool<Balloon>(balloon, balloonContainer, PoolSize, true);
 
             StartCoroutine(SpawnBalloons());
         }
@@ -24,7 +24,7 @@ namespace Internal.Codebase.BalloonLogic
         {
             while (true)
             {
-                balloonPool.GetFree();
+                BalloonPool.GetFree();
                 yield return new WaitForSeconds(SpawnRate);
             }
         }
