571ddc760184dcf303d3fd1391190dee4b1f0610
Vanchegs <manshin9300@gmail.com>
2024-01-26 17:06:09 +0300

    store update

--- Assets/Internal/Codebase/UILogic/StoreLogic/StoreAnimator.cs
+++ Assets/Internal/Codebase/UILogic/StoreLogic/StoreAnimator.cs
@@ -5,9 +5,20 @@ namespace Internal.Codebase.UILogic.StoreLogic
 {
     public class StoreAnimator : MonoBehaviour
     {
+        private bool isStoreActivate;
+        
         public void MoveStore()
         {
-            transform.DOMoveX(2, 1, false);
+            if (isStoreActivate == false)
+            {
+                transform.DOMoveX(0, 1, false);
+                isStoreActivate = true;
+            }
+            else
+            {
+                transform.DOMoveX(17.8f, 1, false);
+                isStoreActivate = false;
+            }
         }
     }
 }
