1394e96c1a165a34a1cc7727f252334450c4ab92
Vanchegs <manshin9300@gmail.com>
2024-02-05 22:48:01 +0300

    Store Ui update

--- Assets/Internal/Codebase/UILogic/StoreLogic/StoreSign.cs
+++ Assets/Internal/Codebase/UILogic/StoreLogic/StoreSign.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Internal.Codebase.UILogic.StoreLogic
@@ -7,9 +8,11 @@ namespace Internal.Codebase.UILogic.StoreLogic
     public class StoreSign : MonoBehaviour
     {
         [field: SerializeField] public bool IsBuy { get; private set; }
+        [field: SerializeField] public int ProductPrice { get; private set; }
         
-        [SerializeField] private TMP_Text nameStoreSign;
-        [SerializeField] private TMP_Text descriptionStoreSign;
+        [SerializeField] private TMP_Text nameStoreSignText;
+        [SerializeField] private TMP_Text descriptionStoreSignText;
+        [SerializeField] private TMP_Text buyButtonText;
         
         [SerializeField] private Image imageStoreSign;
 
@@ -18,13 +21,18 @@ namespace Internal.Codebase.UILogic.StoreLogic
         private void Start()
         {
             CheckIsBuy();
+
+            SettingPrice();
         }
+
+        private void SettingPrice() => 
+            buyButtonText.text = $"{ProductPrice}";
 
         private void CheckIsBuy()
         {
             if (IsBuy) return;
-            nameStoreSign.gameObject.SetActive(false);
-            descriptionStoreSign.gameObject.SetActive(false);
+            nameStoreSignText.gameObject.SetActive(false);
+            descriptionStoreSignText.gameObject.SetActive(false);
             imageStoreSign.gameObject.SetActive(false);
         }
     }
