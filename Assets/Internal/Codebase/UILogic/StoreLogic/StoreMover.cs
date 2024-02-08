b84aa62071bd70bedc0ba763e375d04a89185e80
Vanchegs <manshin9300@gmail.com>
2024-02-09 00:29:02 +0300

    fix bug

--- Assets/Internal/Codebase/UILogic/StoreLogic/StoreMover.cs
+++ Assets/Internal/Codebase/UILogic/StoreLogic/StoreMover.cs
@@ -1,31 +1,37 @@
+
 using System.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Internal.Codebase.UILogic.StoreLogic
 {
     public class StoreMover : MonoBehaviour
     {
-        private Image storeUi;
         private bool isStoreActivate;
 
-        private void Start()
-        {
-            storeUi = GetComponent<Image>();
-        }
+        [SerializeField] private GameObject storeCatalog;
+
+        private void Start() =>
+            storeCatalog.SetActive(false);
 
         public async void MoveStore()
         {
             if (isStoreActivate == false)
             {
+                storeCatalog.SetActive(true);
+                
                 transform.DOMoveX(0, 1, false);
                 isStoreActivate = true;
             }
             else
             {
-                transform.DOMoveX(17.78f, 1, false);
+                transform.DOMoveX(17.78f, 1f, false);
                 isStoreActivate = false;
+
+                await Task.Delay(800);
+                
+                if (isStoreActivate == false)
+                    storeCatalog.SetActive(false);
             }
         }
     }
